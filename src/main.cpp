b9812b35e22af204d3f19b47997c9c66547d6ae4
Faris Azim <aisridzuan117@gmail.com>
2026-09-19 16:41:17 -0500

    feat: print function in main to print ciphers in hex and reset stream to decimal

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,17 @@
 #include <vector>
 #include <memory>
 #include "XorCipher.hpp"
+using namespace std;
 
+int print(const std::vector<uint8_t>& text){
+    for (uint8_t t :text){
+        std::cout << std::hex     << std::setw(2)    << std::setfill('0')        << int(t) << " ";
+        //       set stream to hex   set width to 2     fill empty space with 0
+    }
+    std::cout << std::dec <<std::endl; //reset stream to decimal
+}
+
+int main(int argc, char* argv[]) {
+    
+    return 0; 
+}
